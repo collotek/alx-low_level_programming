202057adafa9d893d4b742150a3fc2a1d326a073
collotek <collinskipkorir94@gmail.com>
2022-12-14 19:54:14 +0300

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * main - prints _putchar
  *
- * Description: prints _putchar
+ * Description: prints putchar
  *
  * Return: 0 (Success)
  */
@@ -10,9 +11,14 @@
 int main(void)
 {
 	char text[10] = "_putchar";
-	_putchar(text);
-	_putchar('\n');
-	return (0);
+	int i = 0;
 
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
 
