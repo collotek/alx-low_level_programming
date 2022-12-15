1e5ea99a7eb4e646b467cbb82a496da2767d5d72
collotek <collinskipkorir94@gmail.com>
2022-12-16 00:14:13 +0300

    sum of numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers- print sum(0-9)
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int sum = 0;
+	int n;
+
+	for ( n = 0; n <= 9; n++ ) 
+	{
+		sum = sum + n;
+
+		return (sum);
+
+	}
+}
+
+
