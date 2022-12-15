e5a5db73026848afa7c38c3910d97ab914a34feb
collotek <collinskipkorir94@gmail.com>
2022-12-16 00:26:46 +0300

    print numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers- print sum(0-9)
- *
- * Return: Always 0.
+ * print_numbers- print numbers(0-9)
  */
 void print_numbers(void)
 {
-	int sum = 0;
 	int n;
 
 	for ( n = 0; n <= 9; n++ ) 
 	{
-		sum = sum + n;
-		
-		_putchar("%d", sum);
+
+		_putchar("%d", n);
 
 	}
 	_putchar("\n");
