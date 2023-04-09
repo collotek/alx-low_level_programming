5c275ac5707eafca4526c47fdd0c8ffef5e2d112
collotek <collinskipkorir94@gmail.com>
2023-04-09 18:54:53 +0300

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
-		free(value_copy);
+		free(copy_of_value);
 		return (0);
 	}
 	new->key = strdup(key);
