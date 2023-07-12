4fe1a43e6831145c2a8b10808423a23b79de51dd
collotek <collinskipkorir94@gmail.com>
2023-07-12 22:26:55 +0300

    requirement corrections

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,24 +3,23 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value)
-{	
+{
 	int brek = 0;
 	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array [i] == value)
+		if (array[i] == value)
 		{
-			return i;
+			return (i);
 			brek = 1;
-			break;	
+			break;
 		}
 	}
-
 	if ((brek == 0) | (array == NULL))
 		return (-1);
-return -1;	
+return (-1);
 }
 
 
