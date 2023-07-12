f600555bcf2383fe0fed8ebd65964fc7d44080e5
collotek <collinskipkorir94@gmail.com>
2023-07-12 22:49:43 +0300

    betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * linear_search - searches value in array using Linear search algorithm
+ * using linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: index where value is found, return -1 if array Null
+ * present or array null
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
 	int brek = 0;
