eb9839dd6844f1c687b208b8df36dc276e3fcd68
collotek <collinskipkorir94@gmail.com>
2023-07-12 17:30:40 +0300

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value)
+{	
+	int brek = 0;
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array [i] == value)
+		{
+			return i;
+			brek = 1;
+			break;	
+		}
+	}
+
+	if ((brek == 0) | (array == NULL))
+		return (-1);
+return -1;	
+}
+
+
